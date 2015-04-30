ebd8e34b68feb3c2f204994d37126e3c9a298ab5
Arjun Munji <arjunmunji@gmail.com>
2015-04-30 23:56:55 +0530

    update

--- kmitra.app.js
+++ kmitra.app.js
@@ -6,17 +6,20 @@ $.getJSON(current_url, function(data) {
 	document.getElementById("content").innerHTML=data.post.content;
 });
 function navToPrevious(){
+window.scrollTo(0, 0);
+document.getElementById("content").innerHTML='<p style="font-size:2em;text-align:center;">Loading...<br/>Please wait.</p>';
 current_url = globalData.previous_url+'?json=1';
 $.getJSON(current_url, function(data) {
 	window.globalData = data;
 	document.getElementById("title").innerHTML=data.post.title;
 	document.getElementById("byline").innerHTML='&mdash; '+data.post.author.name;
 	document.getElementById("content").innerHTML=data.post.content;
-    window.scrollTo(0, 0);
 });
 current_url=globalData.previous_url+'?json=1';
 }
 function navToNext(){
+window.scrollTo(0, 0);
+document.getElementById("content").innerHTML='<p style="font-size:2em;text-align:center;">Loading...<br/>Please wait.</p>';
 if(globalData.next_url){
 current_url = globalData.next_url+'?json=1';
 $.getJSON(current_url, function(data) {
@@ -24,8 +27,6 @@ $.getJSON(current_url, function(data) {
 	document.getElementById("title").innerHTML=data.post.title;
 	document.getElementById("byline").innerHTML='&mdash; '+data.post.author.name;
 	document.getElementById("content").innerHTML=data.post.content;
-    window.scrollTo(0, 0);
-
 });
 current_url=globalData.next_url+'?json=1';
 }
