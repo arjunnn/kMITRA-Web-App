443438181ed7a6c93e7d56cffccdbb637539ef6c
Arjun Munji <arjunmunji@hotmail.com>
2015-05-24 00:19:52 +0530

    tight Tight TIGHT

--- kmitra.app.js
+++ kmitra.app.js
@@ -1,17 +1,11 @@
 window.current_url = 'http://kmit.in/emagazine/article/periscope-vs-meerkat/?json=1';
-$.getJSON(current_url, function(data) {
-	window.globalData = data;
-	document.getElementById("title").innerHTML=data.post.title;
-	document.getElementById("byline").innerHTML='&mdash; '+data.post.author.name;
-	document.getElementById("content").innerHTML=data.post.content;
-});
 function navToPrevious(){
 current_url = globalData.previous_url+'?json=1';
 fillArticle(current_url,0);
 }
 function navToNext(){
 current_url = globalData.next_url+'?json=1';
-fillArticle(current_url,1)
+fillArticle(current_url,1);
 }
 function fillArticle(current_url,p){
 	$.getJSON(current_url, function(data){
