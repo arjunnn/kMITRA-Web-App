8c90515437c3fc0d1db623443288827e76cd8d18
Arjun Munji <arjunmunji@hotmail.com>
2015-05-26 00:44:19 +0530

    :dart:

--- assets/kmitra.app.js
+++ assets/kmitra.app.js
@@ -6,27 +6,27 @@ $.getJSON(current_url, function(data) {
 	document.getElementById("content").innerHTML=data.post.content;
 });
 function navToPrevious(){
-current_url = globalData.previous_url+'?json=1';
-$.getJSON(current_url, function(data) {
 	window.scrollTo(0, 0);
-	document.getElementById("content").innerHTML='<p style="font-size:2em;text-align:center;">Loading...<br/>Please wait.</p>';
-	window.globalData = data;
-	document.getElementById("title").innerHTML=data.post.title;
-	document.getElementById("byline").innerHTML='&mdash; '+data.post.author.name;
-	document.getElementById("content").innerHTML=data.post.content;
-});
+	current_url = globalData.previous_url+'?json=1';
+	$.getJSON(current_url, function(data) {
+		document.getElementById("content").innerHTML='<p style="font-size:2em;text-align:center;">Loading...<br/>Please wait.</p>';
+		window.globalData = data;
+		document.getElementById("title").innerHTML=data.post.title;
+		document.getElementById("byline").innerHTML='&mdash; '+data.post.author.name;
+		document.getElementById("content").innerHTML=data.post.content;
+	});
 current_url=globalData.previous_url+'?json=1';
 }
 function navToNext(){
-current_url = globalData.next_url+'?json=1';
-$.getJSON(current_url, function(data) {
-	document.getElementById("content").innerHTML='<p style="font-size:2em;text-align:center;">Loading...<br/>Please wait.</p>';
 	window.scrollTo(0, 0);
-	window.globalData = data;
-	document.getElementById("title").innerHTML=data.post.title;
-	document.getElementById("byline").innerHTML='&mdash; '+data.post.author.name;
-	document.getElementById("content").innerHTML=data.post.content;
-});
-current_url=globalData.next_url+'?json=1';
+	current_url = globalData.next_url+'?json=1';
+	$.getJSON(current_url, function(data) {
+		document.getElementById("content").innerHTML='<p style="font-size:2em;text-align:center;">Loading...<br/>Please wait.</p>';
+		window.globalData = data;
+		document.getElementById("title").innerHTML=data.post.title;
+		document.getElementById("byline").innerHTML='&mdash; '+data.post.author.name;
+		document.getElementById("content").innerHTML=data.post.content;
+	});
+	current_url=globalData.next_url+'?json=1';
 }
 
